663752cd24cb2dae621beb5bdc6ed27b0393ebbf
Luis Goncalves <luis.goncalves@jumia.com>
2023-04-26 21:09:22 +0100

    Added song persistance to bot

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import re
+import pymongo
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 import requests
@@ -18,6 +20,37 @@ YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 # Define the API key for Telegram
 TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
 
+# Domains divided by ;
+IGNORED_DOMAINS = os.environ['IGNORED_DOMAINS']
+
+# Mongo for data persistance
+MONGO_HOST = os.environ['MONGO_HOST']
+MONGO_PORT = os.environ['MONGO_PORT']
+
+client = pymongo.MongoClient(f'mongodb://{MONGO_HOST}:{MONGO_PORT}/')
+db = client.bangerlink
+collection = db.bangerlink
+
+def save_song(chat_id, user, song_title, artist, youtube_url):
+    """
+    Saves music entry data in db.
+    """
+    music_data = {
+        "chat": chat_id,
+        "date": datetime.utcnow(),
+        "user": {
+            "id": user.id,
+            "name": user.first_name
+        },
+        "title": song_title,
+        "artist": artist,
+        "youtube": youtube_url,
+        "hits": 1
+    }
+
+    result = collection.insert_one(music_data)
+
+    logger.info(f"New song added {result}")
 
 def search_song_on_youtube(song_title, artist):
     """
@@ -26,42 +59,45 @@ def search_song_on_youtube(song_title, artist):
     # Build the query string
     query = song_title + " " + artist
     query = query.replace(" ", "+")
-    
+
     # Make the request to the YouTube API
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={query}&type=video&key={YOUTUBE_API_KEY}"
     response = requests.get(url)
     data = response.json()
-    
+
     # Get the first result
     result = data['items'][0]
     youtube_url = f"https://www.youtube.com/watch?v={result['id']['videoId']}"
-    
+
     return youtube_url
+
 
 def extract_song_info_from_apple_music_link(link):
     """
     Extracts the song title and artist from an Apple Music link.
     """
 
-    ## Split the URL into the resource name and the rest of the path
+    # Split the URL into the resource name and the rest of the path
     protocol, resource_name_and_rest = link.split('://')
-    resource_name, language, album, title, *rest = resource_name_and_rest.split('/')
-    
+    resource_name, language, album, title, * \
+        rest = resource_name_and_rest.split('/')
+
     # Fetch the webpage
     response = requests.get(link)
-    
+
     # Parse the response
     soup = BeautifulSoup(response.text, "html.parser")
-    
+
     title_element = title
 
     # Find the anchor element with the class "metadata-lockup__subheadline"
     artist_element = soup.find('a', class_='click-action')
-    
+
     song_title = title_element.strip()
     artist = artist_element.text.strip()
-    
+
     return song_title, artist
+
 
 def extract_song_info_from_spotify_link(link):
     """
@@ -75,21 +111,21 @@ def extract_song_info_from_spotify_link(link):
     # Construct the modified URL with /embed after the resource name
     modified_link = f'{protocol}://{resource_name}/embed/{"/".join(rest)}'
 
-
     # Fetch the webpage
     response = requests.get(modified_link)
-    
+
     # Parse the response
     soup = BeautifulSoup(response.text, "html.parser")
-    
+
     # Find all the anchor elements in the webpage
     anchors = soup.find_all('a')
-    
+
     # Extract the text from the first two anchor elements
     song = anchors[0].text
     artist = anchors[1].text
 
     return song, artist
+
 
 def search_song(bot, update):
     """
@@ -98,24 +134,50 @@ def search_song(bot, update):
     """
     # Get the message and extract the link
     message = update.message.text
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+
     link = re.search(r"(https?://[^\s]+)", message).group(0)
-    
+
     # Extract the song title and artist from the link
     if "apple.com" in link:
         song_title, artist = extract_song_info_from_apple_music_link(link)
     elif "spotify.com" in link:
         song_title, artist = extract_song_info_from_spotify_link(link)
-    elif "youtube.com" in link:
+    elif any(domain in link for domain in IGNORED_DOMAINS.split(";")):
         return
     else:
-        update.message.reply_text("Sorry, I only support Apple Music (apple.com) and Spotify links (spotify.com), tell Luis to not be lazy and grow my code.")
+        update.message.reply_text(
+            f"Sorry {user.first_name}, I only support Apple Music (apple.com) and Spotify links (spotify.com), tell Luis to not be lazy and grow my code.")
         return
-    
+
+    query = {
+        "title": song_title,
+        "artist": artist,
+        "chat_id": chat_id
+    }
+    result = collection.find_one(query)
+    if result is not None:
+        date_str = result["date"].strftime("%d of %m in %Y")
+        
+        update.message.reply_text(
+            f'This banger has been mentioned here {result["hits"]} times. {result["user"]["name"]} first mentioned this banger in this chat {date_str}! \n{result["youtube"]}')
+
+        newhits = result["hits"] + 1
+        update_query = {"$set": {"hits": newhits}}
+        collection.update_one(query, update_query)
+
+        return
+
     # Search YouTube for the song
     youtube_url = search_song_on_youtube(song_title, artist)
     
+    save_song(chat_id,user, song_title, artist, youtube_url)
+    
     # Send the YouTube link as a message
-    update.message.reply_text(f'Here is the Youtube Link, keep on bangin\' 😎\n{youtube_url}')
+    update.message.reply_text(
+        f'Here is the Youtube Link, keep on bangin\' 😎\n{youtube_url}')
+
 
 def main():
     # Create the Updater and pass it the API key
@@ -134,6 +196,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
